e07e52c9d1a25b40fe9513dc87a8d78fe3cfff09
Tyler-Taaca <tylertaaca@gmail.com>
2021-05-12 20:18:40 -0400

    commit to master

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar (){
             <li><Link to="/" className="navbar-item">Home</Link></li>
             <li><Link to="/about" className="navbar-item">About</Link></li>
             <li><Link to="/work" className="navbar-item">Work and Projects</Link></li>
-            <a href='https://drive.google.com/file/d/14c8hP0G3FIJTZ_22gFTv_XD-06lX_Bz9/view?usp=sharing' className='no'><li className="navbar-item">Resume</li></a>
+            <a href='https://drive.google.com/file/d/1MfC2ZeYq-jaYmLgW7jtlQJQMbIkwWodj/view?usp=sharing' className='no'><li className="navbar-item">Resume</li></a>
             <li><Link to="/contact" className="navbar-item">Contact</Link></li>
         </section>
     )
